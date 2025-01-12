9099f7c163702f63366a4a2a472fdb9d53f7d157
Matt <157317090+pan-kroczek@users.noreply.github.com>
2025-01-12 19:06:36 +0100

    Basic equations added to JS

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(1, 2));
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subtract(3, 4));
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(2, 4));
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divide(4, 2));
