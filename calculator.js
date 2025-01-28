32bc56443ec446bfdd066e372ad71acc7db67fd1
Matt <157317090+pan-kroczek@users.noreply.github.com>
2025-01-28 17:34:08 +0100

    Calculator can add elements
    Add button and equals button are functional

--- calculator.js
+++ calculator.js
@@ -2,52 +2,96 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
-console.log(add(1, 2));
-
 function subtract(num1, num2) {
     return num1 - num2;
 }
-
-console.log(subtract(3, 4));
 
 function multiply(num1, num2) {
     return num1 * num2;
 }
 
-console.log(multiply(2, 4));
-
 function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(divide(4, 2));
-
 var firstNumber;
 var operator;
 var secondNumber;
-var equation = '';
+var currentNumber = '';
 
 const result = document.querySelector('#result');
-
 const buttons = document.querySelector('#buttons');
+const addButton = buttons.querySelector('#add');
+const subtractButton = buttons.querySelector('#subtract');
+const multiplyButton = buttons.querySelector('#multiply');
+const divideButton = buttons.querySelector('#divide');
+const equalButton = buttons.querySelector('#equals');
+
+// buttons.addEventListener('click', (event) => {
+//     const isNumber = event.target.className === 'number';
+//     const isOperator = event.target.className === 'operator';
+//     if (!isNumber && !isOperator) {
+//       return;
+//     }
+  
+//     result.textContent += event.target.value;
+//     currentNumber = result.textContent;
+//     console.log(currentNumber);
+//   });
 
 buttons.addEventListener('click', (event) => {
     const isNumber = event.target.className === 'number';
-    const isOperator = event.target.className === 'operator';
-    if (!isNumber && !isOperator) {
+    if (!isNumber) {
       return;
     }
-  
+    
     result.textContent += event.target.value;
-    equation = result.textContent;
-    console.log(equation);
+    currentNumber = result.textContent;
+    console.log(currentNumber);
+    addButton.disabled = false;
+  });
+
+addButton.addEventListener('click', (event) => {
+    if (firstNumber === undefined) {
+        firstNumber = Number(currentNumber);
+    } else {
+        firstNumber = add(firstNumber, Number(currentNumber));
+    }
+    clearScreen();
+    operator = '+';
+    console.log(firstNumber);
+    console.log(operator);
+    addButton.disabled = true;
+  });
+
+  equalButton.addEventListener('click', () => {
+    if (firstNumber === undefined) {
+        return;
+    } else if (operator === '+') {
+        secondNumber = Number(currentNumber);
+        result.textContent = `${add(firstNumber, secondNumber)}`;
+        addButton.disabled = false;
+    } else if (operator === '-') {
+        secondNumber = Number(currentNumber);
+        result.textContent = `${subtract(firstNumber, secondNumber)}`;
+    } else if (operator === '*') {
+        secondNumber = Number(currentNumber);
+        result.textContent = `${multiply(firstNumber, secondNumber)}`;
+    } else if (operator === '/') {
+        secondNumber = Number(currentNumber);
+        result.textContent = `${divide(firstNumber, secondNumber)}`;
+    }
   });
 
 const clear = document.querySelector('#clear');
 
 function clearScreen() {
     result.textContent = '';
-    equation = '';
+    currentNumber = '';
 }
 
-clear.addEventListener('click', clearScreen);
+clear.addEventListener('click', () => {
+    clearScreen();
+    firstNumber = undefined;
+    operator = undefined;
+});
